09ca2a14d1a9ecdd7486e98decf7e0c59d3bc953
unknown <anna.watzin@gmail.com>
2026-03-20 20:38:32 +0200

    puskane

--- acktesttues/acktesttues/settings.py
+++ acktesttues/acktesttues/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i8*kr7s=*d!&!*98212ksb966(h+8@t7f&7wbmfpehen!fer9+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['MEAh1.pythonanywhere.com']
 
 
 # Application definition
